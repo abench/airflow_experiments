3a69d70755d85a03f3756c3e41d4f45744db8347
Andriy Bench <andriy.bench@gmail.com>
2022-07-19 01:04:47 +0300

    Templates added to download_rss_feed.py

--- download_rss_feed.py
+++ download_rss_feed.py
@@ -16,28 +16,34 @@ dag = DAG(
 )
 
 download_rss_file = BashOperator(
-    task_id="download_rsslaunches",
-    bash_command="curl -o /tmp/rss.xml -L 'https://tokar.ua/feed'",
+    task_id="download_rss_file",
+    bash_command="curl -o /tmp/rss_{{ds}}.xml -L 'https://tokar.ua/feed'",
     dag=dag,
 )
 
 
-def _xml_to_json():
+def _xml_to_json(**kwargs):
     # Download all pictures in launches.json
-    with open("/tmp/rss.xml") as fi:
-        with open("/tmp/rss.json", "w") as fo:
+    input_file_name = kwargs["input_file"]
+    output_file_name = kwargs["output_file"]
+    with open(input_file_name) as fi:
+        with open(output_file_name, "w") as fo:
             json.dump(fi.read(), fo)
 
 
 xml_to_json = PythonOperator(
     task_id="xml_to_json",
     python_callable=_xml_to_json,
+    op_kwargs={
+        "input_file":"/tmp/rss_{{ ds }}.xml",
+        "output_file":"/tmp/rss_{{ ds }}.json",
+    },
     dag=dag,
 )
 
 notify = BashOperator(
     task_id="notify",
-    bash_command='cat /tmp/rss.json',
+    bash_command='cat /tmp/rss_{{ds}}.json',
     dag=dag,
 )
 
